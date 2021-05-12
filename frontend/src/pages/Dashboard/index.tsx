41cca2c9343414b892a1bf0d42178608a165973a
unknown <jpmoreiradejesus@hotmail.com>
2021-05-12 17:04:05 -0300

    Env var

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,7 +1,7 @@
 import BarChart from "components/BarChart";
 import DataTable from "components/DataTable";
 import DonutChart from "components/DonutChart";
-import Footer from "components/footer";
+import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 
 const Dashboard = () => {
